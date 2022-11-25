f8b0cfddbae64a1f315e717bfbafe50d346217ca
Fekete András <andris2583@gmail.com>
2022-11-25 23:30:48 +0100

    Searchbar, no functionality

--- src/app/modules/header/header.module.ts
+++ src/app/modules/header/header.module.ts
@@ -1,26 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header.component';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {RouterModule} from "@angular/router";
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {RouterModule} from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {SearchbarComponent} from './searchbar/searchbar.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  exports: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    RouterModule
-  ]
+    declarations: [
+        HeaderComponent,
+        SearchbarComponent
+    ],
+    exports: [
+        HeaderComponent
+    ],
+    imports: [
+        CommonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        RouterModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule
+    ]
 })
-export class HeaderModule { }
+export class HeaderModule {
+}
---
+++ src/app/modules/header/searchbar/searchbar.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
+
+@Component({
+    selector: 'app-searchbar',
+    templateUrl: './searchbar.component.html',
+    styleUrls: ['./searchbar.component.scss'],
+    animations: [
+        trigger(
+            'inOutAnimation',
+            [
+                transition(
+                    ':enter',
+                    [
+                        style({height: 0, opacity: 1}),
+                        animate('250ms ease-out',
+                            style({height: '*', opacity: 1}))
+                    ]
+                ),
+                transition(
+                    ':leave',
+                    [
+                        style({height: '*', opacity: 1}),
+                        animate('250ms ease-in',
+                            style({height: 0, opacity: 1}))
+                    ]
+                )
+            ]
+        )
+    ]
+})
+export class SearchbarComponent implements OnInit {
+
+    constructor() {
+    }
+
+    searchbarInFocus: boolean = false;
+
+    searchbarValue: string = '';
+
+    ngOnInit(): void {
+    }
+
+    onSearchFocus() {
+        this.searchbarInFocus = true;
+    }
+
+    onSearchBlur() {
+        this.searchbarInFocus = false;
+        this.searchbarValue = '';
+    }
+
+    onKeyUp($event: KeyboardEvent) {
+    }
+
+    onKeyDown($event: KeyboardEvent) {
+        if ($event.code == 'Escape') {
+            this.onSearchBlur();
+            this.searchbarValue = '';
+            let form = document.getElementById('search-input');
+            form?.blur();
+        }
+    }
+}
